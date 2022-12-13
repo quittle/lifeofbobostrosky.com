0ac5e99dd3425fc96f7a3e78ed80b64f5682c228
Dustin Toff <me@dustintoff.com>
2022-12-12 22:18:52 -0500

    Add data download script

---
+++ scripts/download-data.ts
@@ -0,0 +1,194 @@
+#!./node_modules/.bin/ts-node
+
+/* eslint-disable no-console */
+
+import * as fsSync from "fs";
+import AWS, { S3 } from "aws-sdk";
+import { promises as fs } from "fs";
+import os from "os";
+import path from "path";
+import process from "process";
+
+const STACK_NAME = "lifeofbobostrosky-com";
+
+enum Resource {
+  UpdatesTable = "UpdatesTable",
+  MemorialWallTable = "MemorialWallTable",
+  MemorialWallBucket = "MemorialWallBucket",
+}
+
+async function getStorageFolder(stackName: string): Promise<string> {
+  const folder = path.join(os.homedir(), "Downloads", `${stackName}-download`);
+  await fs.mkdir(folder, { recursive: true });
+  return folder;
+}
+async function getMemorialWallFolder(stackName: string): Promise<string> {
+  const folder = path.join(await getStorageFolder(stackName), "memorial-wall");
+  fs.mkdir(folder, { recursive: true });
+  return folder;
+}
+
+function assertNotNull<T>(value: T, errorMessage?: string): NonNullable<T> {
+  if (value !== null && value !== undefined) {
+    return value;
+  }
+
+  throw new Error("Value was null: " + errorMessage);
+}
+
+async function scanTable(
+  params: AWS.DynamoDB.ScanInput
+): Promise<AWS.DynamoDB.AttributeMap[]> {
+  const dynamodb = new AWS.DynamoDB();
+
+  const scanResults: AWS.DynamoDB.AttributeMap[] = [];
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    // eslint-disable-next-line no-await-in-loop
+    const items = await dynamodb.scan(params).promise();
+    if (items.$response.error) {
+      throw items.$response.error;
+    }
+    items?.Items?.forEach((item) => scanResults.push(item));
+    const lastEvaluatedKey = items?.LastEvaluatedKey;
+    if (lastEvaluatedKey === undefined) {
+      break;
+    }
+    // eslint-disable-next-line require-atomic-updates
+    params.ExclusiveStartKey = lastEvaluatedKey;
+  }
+  return scanResults;
+}
+
+async function downloadUpdates(
+  stackName: string,
+  cloudformation: AWS.CloudFormation
+): Promise<void> {
+  const update = await cloudformation
+    .describeStackResource({
+      StackName: stackName,
+      LogicalResourceId: Resource.UpdatesTable,
+    })
+    .promise();
+  const tableName = assertNotNull(
+    update.StackResourceDetail?.PhysicalResourceId
+  );
+
+  const entries = await scanTable({ TableName: tableName });
+
+  const updatesFile = path.join(
+    await getStorageFolder(stackName),
+    "updates.json"
+  );
+
+  const serializedUpdates = JSON.stringify(entries);
+  await fs.writeFile(updatesFile, serializedUpdates);
+
+  console.info(`Wrote ${serializedUpdates.length} chars to ${updatesFile}`);
+}
+
+async function downloadMemorialWallFile(
+  stackName: string,
+  bucketName: string,
+  key: string
+): Promise<void> {
+  const folderName = await getMemorialWallFolder(stackName);
+  const fileName = path.join(folderName, key);
+  if (fsSync.existsSync(fileName)) {
+    console.debug(`${fileName} already exists on disk.`);
+    return;
+  }
+
+  console.info(`Downloading ${fileName} to disk.`);
+
+  let object;
+  try {
+    object = await new S3()
+      .getObject({ Bucket: bucketName, Key: key })
+      .promise();
+  } catch (err) {
+    if (err instanceof Error && err.name === "NoSuchKey") {
+      console.warn(`s3:\\${bucketName}/${fileName} does not exist`);
+      await fs.writeFile(
+        fileName,
+        `This file did not exist at ${new Date().toLocaleString()}`
+      );
+      return;
+    }
+    throw err;
+  }
+
+  await fs.writeFile(
+    fileName,
+    // @ts-expect-error Blob is incompatible
+    assertNotNull(object.Body, "S3 object body null")
+  );
+}
+
+async function getMemorialWallBucketName(
+  stackName: string,
+  cloudformation: AWS.CloudFormation
+): Promise<string> {
+  const update = await cloudformation
+    .describeStackResource({
+      StackName: stackName,
+      LogicalResourceId: Resource.MemorialWallBucket,
+    })
+    .promise();
+
+  return assertNotNull(
+    update.StackResourceDetail?.PhysicalResourceId,
+    `Unable to find ${Resource.MemorialWallBucket}`
+  );
+}
+
+async function downloadMemorialWall(
+  stackName: string,
+  cloudformation: AWS.CloudFormation
+): Promise<void> {
+  const bucketName = await getMemorialWallBucketName(stackName, cloudformation);
+  const update = await cloudformation
+    .describeStackResource({
+      StackName: stackName,
+      LogicalResourceId: Resource.MemorialWallTable,
+    })
+    .promise();
+  const tableName = assertNotNull(
+    update.StackResourceDetail?.PhysicalResourceId
+  );
+
+  const entries = await scanTable({ TableName: tableName });
+
+  const memorialWallFile = path.join(
+    await getStorageFolder(stackName),
+    "memorial-wall.json"
+  );
+
+  const serializedUpdates = JSON.stringify(entries);
+  await fs.writeFile(memorialWallFile, serializedUpdates);
+
+  console.info(
+    `Wrote ${serializedUpdates.length} chars to ${memorialWallFile}`
+  );
+  entries.flatMap((entry) =>
+    assertNotNull(entry["files"].SS, "Required non-null string set")
+      .filter((file) => file !== "")
+      .map((file) => downloadMemorialWallFile(stackName, bucketName, file))
+  );
+}
+
+async function main() {
+  AWS.config.update({ region: "us-east-1" });
+  const stackName = process.env["STACK_NAME"] ?? STACK_NAME;
+
+  const cloudformation = new AWS.CloudFormation();
+  await Promise.all([
+    downloadUpdates(stackName, cloudformation),
+    downloadMemorialWall(stackName, cloudformation),
+  ]);
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
